2d3982c50710ade16f354f019c977f9635358f77
Ronny Trommer <ronny@opennms.org>
2015-09-19 23:51:32 +0200

    Fix cookbook name for powerdns and close issue #4

--- cookbooks/powerdns/metadata.rb
+++ cookbooks/powerdns/metadata.rb
@@ -4,10 +4,10 @@ license          "GPLv3+"
 description      "Installs/Configures PowerDNS for ddserver"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
-name             "PowerDNS for ddserver"
+name             "powerdns"
 provides         "PowerDNS for ddserver"
 
-recipe "ddserver", "Installs PowerDNS and configuration for ddserver"
+recipe "powerdns", "Installs PowerDNS and configuration for ddserver"
 
 %w{ centos }.each do |os|
     supports os
